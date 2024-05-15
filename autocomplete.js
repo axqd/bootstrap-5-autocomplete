74384d7a855ca2933cabe02638378562bd4abff4
Tian You <axqd001@gmail.com>
2024-05-15 19:33:32 +0100

    Simplified version

--- autocomplete.js
+++ autocomplete.js
@@ -1,100 +1,103 @@
-import * as bootstrap from 'bootstrap'
+class AutoComplete {
+  static DEFAULTS = Object.freeze({
+    minNumOfCharsToMatch: 3,
+    maxNumOfItems: 5,
+    highLightTyped: true,
+    highLightClass: 'text-primary',
+  });
 
-const DEFAULTS = {
-  threshold: 2,
-  maximumItems: 5,
-  highlightTyped: true,
-  highlightClass: 'text-primary',
-  label: 'label',
-  value: 'value',
-  showValue: false,
-  showValueBeforeLabel: false,
-};
-
-class Autocomplete {
   constructor(field, options) {
     this.field = field;
-    this.options = Object.assign({}, DEFAULTS, options);
-    this.dropdown = null;
+    this.options = Object.assign({}, AutoComplete.DEFAULTS, options);
 
     field.parentNode.classList.add('dropdown');
     field.setAttribute('data-bs-toggle', 'dropdown');
     field.classList.add('dropdown-toggle');
 
-    const dropdown = ce(`<div class="dropdown-menu"></div>`);
-    if (this.options.dropdownClass)
-      dropdown.classList.add(this.options.dropdownClass);
+    this.insertAfter(this.createDropdownMenu(), field);
 
-    insertAfter(dropdown, field);
-
-    this.dropdown = new bootstrap.Dropdown(field, this.options.dropdownOptions);
+    this.dropdown = new bootstrap.Dropdown(field);
 
     field.addEventListener('click', (e) => {
       if (this.createItems() === 0) {
-        e.stopPropagation();
         this.dropdown.hide();
       }
     });
 
-    field.addEventListener('input', () => {
-      if (this.options.onInput)
-        this.options.onInput(this.field.value);
-      this.renderIfNeeded();
+    field.addEventListener('input', (e) => {
+      this.render(e);
     });
 
     field.addEventListener('keydown', (e) => {
-      if (e.keyCode === 27) {
+      if (e.keyCode === 27) { // Esc
         this.dropdown.hide();
         return;
       }
-      if (e.keyCode === 40) {
+      if (e.keyCode === 40) { // ArrowDown
         this.dropdown._menu.children[0]?.focus();
         return;
       }
     });
   }
 
-  setData(data) {
-    this.options.data = data;
-    this.renderIfNeeded();
+  createDropdownMenu() {
+    const div = document.createElement('div');
+    div.classList.add('dropdown-menu');
+    return div;
   }
 
-  renderIfNeeded() {
-    if (this.createItems() > 0)
-      this.dropdown.show();
-    else
-      this.field.click();
-  }
+  createButton(lookup, item) {
+    const btn = document.createElement('button');
+    btn.classList.add('dropdown-item');
+    btn.setAttribute('data-label', item);
 
-  createItem(lookup, item) {
-    let label;
-    if (this.options.highlightTyped) {
-      const idx = removeDiacritics(item.label)
-          .toLowerCase()
-          .indexOf(removeDiacritics(lookup).toLowerCase());
-      const className = Array.isArray(this.options.highlightClass) ? this.options.highlightClass.join(' ')
-        : (typeof this.options.highlightClass == 'string' ? this.options.highlightClass : '');
-      label = item.label.substring(0, idx)
-        + `<span class="${className}">${item.label.substring(idx, idx + lookup.length)}</span>`
-        + item.label.substring(idx + lookup.length, item.label.length);
-    } else {
-      label = item.label;
-    }
+    if (this.options.highLightTyped) {
+      const idx = this.removeDiacritics(item).toLowerCase()
+          .indexOf(this.removeDiacritics(lookup).toLowerCase());
 
-    if (this.options.showValue) {
-      if (this.options.showValueBeforeLabel) {
-        label = `${item.value} ${label}`;
-      } else {
-        label += ` ${item.value}`;
+      const part1 = item.substring(0, idx);
+      const part2 = item.substring(idx, idx + lookup.length);
+      const part3 = item.substring(idx + lookup.length, item.length);
+
+      btn.innerText = part1;
+
+      if (part2.length > 0) {
+        const span = document.createElement('span');
+        span.classList.add(this.options.highLightClass);
+        span.innerText = part2;
+
+        btn.appendChild(span);
       }
+
+      if (part3.length > 0) {
+        btn.appendChild(document.createTextNode(part3));
+      }
+    } else {
+      btn.innerText = item;
     }
 
-    return ce(`<button type="button" class="dropdown-item" data-label="${item.label}" data-value="${item.value}">${label}</button>`);
+    return btn;
+  }
+
+  insertAfter(elem, refElem) {
+    return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+  }
+
+  removeDiacritics(str) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  render(e) {
+    if (this.createItems() > 0) {
+      this.dropdown.show();
+    } else {
+      this.dropdown.hide();
+    }
   }
 
   createItems() {
     const lookup = this.field.value;
-    if (lookup.length < this.options.threshold) {
+    if (lookup.length < this.options.minNumOfCharsToMatch) {
       this.dropdown.hide();
       return 0;
     }
@@ -102,73 +105,31 @@ class Autocomplete {
     const items = this.field.nextSibling;
     items.innerHTML = '';
 
-    const keys = Object.keys(this.options.data);
-
     let count = 0;
-    for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      const entry = this.options.data[key];
-      const item = {
-          label: this.options.label ? entry[this.options.label] : key,
-          value: this.options.value ? entry[this.options.value] : entry
-      };
-
-      if (removeDiacritics(item.label).toLowerCase().indexOf(removeDiacritics(lookup).toLowerCase()) >= 0) {
-        items.appendChild(this.createItem(lookup, item));
-        if (this.options.maximumItems > 0 && ++count >= this.options.maximumItems)
+    for (let i = 0; i < this.options.data.length; i++) {
+      const item = this.options.data[i];
+      if (this.removeDiacritics(item).toLowerCase().indexOf(this.removeDiacritics(lookup).toLowerCase()) >= 0) {
+        items.appendChild(this.createButton(lookup, item));
+        if (this.options.maxNumOfItems > 0 && ++count >= this.options.maxNumOfItems)
           break;
       }
     }
 
-    this.field.nextSibling.querySelectorAll('.dropdown-item').forEach((item) => {
-      item.addEventListener('click', (e) => {
-        let dataLabel = e.currentTarget.getAttribute('data-label');
-        let dataValue = e.currentTarget.getAttribute('data-value');
+    items.addEventListener('click', (e) => {
+      let dataLabel = e.target.getAttribute('data-label');
+      if (dataLabel === null) {
+        dataLabel = e.target.parentElement.getAttribute('data-label');
+      }
 
-        this.field.value = dataLabel;
+      this.field.value = dataLabel;
 
-        if (this.options.onSelectItem) {
-          this.options.onSelectItem({
-            value: dataValue,
-            label: dataLabel
-          });
-        }
+      if (this.options.onSelectItem) {
+        this.options.onSelectItem(dataLabel);
+      }
 
-        this.dropdown.hide();
-      })
+      this.dropdown.hide();
     });
 
     return items.childNodes.length;
   }
 }
-
-/**
- * @param html
- * @returns {Node}
- */
-function ce(html) {
-  let div = document.createElement('div');
-  div.innerHTML = html;
-  return div.firstChild;
-}
-
-/**
- * @param elem
- * @param refElem
- * @returns {*}
- */
-function insertAfter(elem, refElem) {
-  return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
-}
-
-/**
- * @param {String} str
- * @returns {String}
- */
-function removeDiacritics(str) {
-  return str
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '');
-}
-
-export { Autocomplete };
